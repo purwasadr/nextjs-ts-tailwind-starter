fa619ff0b11e331e81b55c66b8e50fc2ec7eed6e
Purwa Sadr <purwasadr@gmail.com>
2023-03-03 10:28:54 +0700

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp')
+  ],
+}
